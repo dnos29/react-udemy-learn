545efcb9d7ae1fbbfcfa8ff10c96302afa1e9be0
Son dang <sondn.da@gmail.com>
2020-08-21 17:34:36 +0700

    2way binddingu

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-const App = () => {
-  const [ people, setPeople ] = useState([
-    { name: 'Dnos', age: 26 },
-    { name: 'Sondn', age: 25 },
-    { name: 'Ignis', age: 20 },
-  ])
-  const switchNameHandler = () => {
-    setPeople([
-      { name: 'sondn', age: 26 },
+
+
+class App extends Component {
+  state = {
+    people: [
+      { name: 'Dnos', age: 26 },
       { name: 'Sondn', age: 25 },
       { name: 'Ignis', age: 20 }
-    ])
+    ]
   }
-  return (
-    <div className="App">
-      <button onClick={switchNameHandler}>Switch name</button>
-      {/* <button onClick={this.switchNameHandler()}>Switch name</button> TODO: don't using function like this, it with be excute immidiately */}
-      <Person name={people[0].name} age={people[0].age}>
-        <i>I'm your son of first person</i>
-      </Person>
-      <Person name={people[1].name} age={people[1].age} />
-      <Person name={people[2].name} age={people[2].age} />
-    </div>
-  )
+  switchNameHandler = () => {
+    this.setState({
+      people: [
+        { name: 'sondn', age: 26 },
+        { name: 'Sondn', age: 25 },
+        { name: 'Ignis', age: 20 }
+      ]
+    })
+  }
+  changeName = (event) => {
+    this.setState({
+      people: [
+        { name: 'sondn', age: 26 },
+        { name: event.target.value, age: 25 },
+        { name: 'Ignis', age: 21 }
+      ]
+    })
+  }
+  render() {
+    const btnStyle = {
+      backgroundColor: 'dodgerblue',
+      color: 'white',
+      padding: '10px',
+      borderRadius: '3px',
+      border: '1px solid dodgerblue'
+    }
+    return (
+      <div  className="App">
+        <button style={btnStyle} onClick={this.switchNameHandler}>Switch name</button>
+        {/* <button onClick={this.switchNameHandler()}>Switch name</button> TODO: don't using function like this, it with be excute immidiately */}
+        <Person name={this.state.people[0].name} age={this.state.people[0].age}>
+          <i>I'm your son of first person</i>
+        </Person>
+        <Person name={this.state.people[1].name} age={this.state.people[1].age} changed={this.changeName} />
+        <Person name={this.state.people[2].name} age={this.state.people[2].age} />
+      </div>
+    )
+  }
 }
-
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,6 +5,7 @@ const Person = props => {
         <div>
             <p>I'm {props.name} and I'm {props.age} year old</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} />
         </div>
     )
 }
