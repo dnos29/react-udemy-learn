af760a2a0025d5d9599936278b5a739c89ac59c5
Son dang <sondn.da@gmail.com>
2020-08-21 11:11:49 +0700

    manupulating state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Person from './Person/Person';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    people: [
+      { name: 'Dnos', age: 26 },
+      { name: 'Sondn', age: 25 },
+      { name: 'Ignis', age: 20 }
+    ]
+  }
+  switchNameHandler = () => {
+    this.setState({
+      people: [
+        { name: 'sondn', age: 26 },
+        { name: 'Sondn', age: 25 },
+        { name: 'Ignis', age: 20 }
+      ]
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        {/* <button onClick={this.switchNameHandler()}>Switch name</button> TODO: don't using function like this, it with be excute immidiately */}
+        <Person name={this.state.people[0].name} age={this.state.people[0].age}>
+          <i>I'm your son of first person</i>
+        </Person>
+        <Person name={this.state.people[1].name} age={this.state.people[1].age} />
+        <Person name={this.state.people[2].name} age={this.state.people[2].age} />
+      </div>
+    )
+  }
 }
-
 export default App;
